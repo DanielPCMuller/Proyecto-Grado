248edd48ccac3d2a55dae8f9ac891b8837af09b5
DanielPCMuller <31355265+DanielPCMuller@users.noreply.github.com>
2021-07-01 17:53:39 -0500

    JJJJ
    Quitar Comillas

--- PymeInventarios/PymeInventarios/CargaSistema.vb
+++ PymeInventarios/PymeInventarios/CargaSistema.vb
@@ -12,9 +12,9 @@
         Timer1.Enabled = True
     End Sub
 
-    'Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
 
-    'End Sub
+    End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         If Contador < 100 Then
